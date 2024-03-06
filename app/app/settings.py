04a83f8f3b7132a7987beae36bb59ebec96a4bd3
LuccaHaddadSerejo <hslucca@gmail.com>
2024-03-06 06:14:51 -0300

    refactor (app-settings): refactored the apps in settings

--- app/app/settings.py
+++ app/app/settings.py
@@ -31,17 +31,25 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+]
+
+THIRD_PARTY_APPS = [
+    'rest_framework',
+]
+
+MY_APPS = [
     'core',
     'codeleap',
-    'rest_framework,
 ]
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + MY_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
